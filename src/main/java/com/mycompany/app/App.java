bd8252f2cdcd5cedef0dc0a7cbbeda268a5676c3
ScarlettOverkill <selen.yel@hotmail.com>
2019-02-02 21:31:09 +0300

    Gettşng ready for deployment to heroku

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,28 +1,74 @@
 package com.mycompany.app;
 
+import static spark.Spark.get;
+import static spark.Spark.port;
+import static spark.Spark.post;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
+
 
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
-        ArrayList<Integer> testListesi = new ArrayList<Integer>();
-        testListesi.add(5);
-        testListesi.add(10);
-        testListesi.add(15);
-        ArrayList<Integer> tersListe = TersCevir(testListesi);
-        for(Integer deger : tersListe){
-            System.out.println(deger);
-        }
+    public static void main(String[] args) {
+        port(getHerokuAssignedPort());
 
+        get("/", (req, res) -> "Hello, World");
+
+        post("/compute", (req, res) -> {
+            //System.out.println(req.queryParams("input1"));
+            //System.out.println(req.queryParams("input2"));
+
+            String input1 = req.queryParams("input1");
+            java.util.Scanner sc1 = new java.util.Scanner(input1);
+            sc1.useDelimiter("[;\r\n]+");
+            java.util.ArrayList<Integer> inputList = new java.util.ArrayList<>();
+            while (sc1.hasNext())
+            {
+                int value = Integer.parseInt(sc1.next().replaceAll("\\s",""));
+                inputList.add(value);
+            }
+            System.out.println(inputList);
+
+
+            String input2 = req.queryParams("input2").replaceAll("\\s","");
+            int input2AsInt = Integer.parseInt(input2);
+
+            ArrayList<Integer> result = App.TersCevir(inputList);
+
+            Map map = new HashMap();
+            map.put("result", result);
+            return new ModelAndView(map, "compute.mustache");
+        }, new MustacheTemplateEngine());
+
+
+        get("/compute",
+                (rq, rs) -> {
+                    Map map = new HashMap();
+                    map.put("result", "not computed yet!");
+                    return new ModelAndView(map, "compute.mustache");
+                },
+                new MustacheTemplateEngine());
     }
 
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
+
     public static ArrayList<Integer> TersCevir(ArrayList<Integer> liste){
-        if(liste.size() < 2) return null;
         ArrayList<Integer> listea = new ArrayList<Integer>();
 
         for(int i = liste.size()-1 ; i >=0 ;i--)
